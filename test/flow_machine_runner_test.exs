a5798a45029d57e22dd63db7b15b8cf4b796c2bb
Simon de Haan <simon@praekelt.org>
2020-12-09 12:28:26 +0100

    load sample flow from viamo

--- test/flow_machine_runner_test.exs
+++ test/flow_machine_runner_test.exs
@@ -2,6 +2,7 @@ defmodule FlowMachine.RunnerTest do
   use ExUnit.Case
   doctest FlowMachine.Runner
   alias FlowMachine.{Context, Runner}
+  import FlowMachine.TestHelpers
 
   def context!(_) do
     {:ok, context: %Context{}}
@@ -11,6 +12,9 @@ defmodule FlowMachine.RunnerTest do
     setup :context!
 
     test "a flow", %{context: context} do
+      [fixture] = load_fixture("priv/fixtures/flows/Flows-1607486601464.json")
+      flow = FlowMachine.Flow.load(fixture)
+
       assert {:ok, cursor} = Runner.run(context)
     end
   end
