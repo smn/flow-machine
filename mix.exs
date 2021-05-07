007aba7f0b1a9a1ca84819cfdade11c392ff2ab6
Simon de Haan <simon@praekelt.org>
2021-05-07 13:20:03 +0200

    include junit output

--- mix.exs
+++ mix.exs
@@ -33,7 +33,8 @@ defmodule FlowMachine.MixProject do
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:expression, "~> 0.2.1"},
       {:uuid, "~> 1.1"},
-      {:jason, "~> 1.2.2"}
+      {:jason, "~> 1.2.2"},
+      {:junit_formatter, "~> 3.1", only: [:test]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
